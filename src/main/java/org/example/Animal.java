d09a00076a418bdaf4c2771d408c74eb11969b14
catsoup88 <yakutingleb@mail.ru>
2025-07-16 17:02:18 +0300

    add new branch with lesson4

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -1,4 +1,19 @@
 package org.example;
 
-public class Animal {
+public abstract class Animal {
+    protected String name;
+    protected static int animalsCount;
+
+    public Animal(String name) {
+        this.name = name;
+        animalsCount++;
+    }
+
+    public abstract void run(int distance);
+
+    public abstract void swim(int distance);
+
+    public static int getAnimalsCount() {
+        return animalsCount;
+    }
 }

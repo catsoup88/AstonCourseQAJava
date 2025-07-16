c8388225a6312597912d6089823b4aeef6ef7b89
catsoup88 <yakutingleb@mail.ru>
2025-07-16 17:23:23 +0300

    add new branch with lesson4

--- task2/src/main/java/org/example/Main.java
+++ task2/src/main/java/org/example/Main.java
@@ -3,9 +3,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Shape circle = new Circle(5, "Синий", "Зелёный");
-        Shape rectangle = new Rectangle(4, 6, "Фиолетовый", "Жёлтый");
-        Shape triangle = new Triangle(3, 4, 5, "Оранжевый", "Коричневый");
+        Shape circle = new Circle(7, "Розовый", "Фиолетовый");
+        Shape rectangle = new Rectangle(5, 8, "Зеленый", "Жёлтый");
+        Shape triangle = new Triangle(5, 3, 9, "Красный", "Синий");
 
         circle.printInfo();
         System.out.println();

42d9bb5ed90d296998ec87022c32c725ad85056a
catsoup88 <yakutingleb@mail.ru>
2025-07-21 19:46:30 +0300

    add new branch with lesson5

--- src/main/java/org/example/Array.java
+++ src/main/java/org/example/Array.java
@@ -1,4 +1,26 @@
 package org.example;
 
 public class Array {
+    public static int Array(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4) {
+            throw new MyArraySizeException("Неверный размер массива, массив должен быть размеров 4х4");
+        }
+        for (String[] abc : array) {
+            if (abc.length != 4) {
+                throw new MyArraySizeException("Неверный размер массива, массив должен быть размеров 4х4");
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Ошибка в ячейке [" + i + "][" + j + "]: некорректное значение '" + array[i][j] + "'");
+                }
+            }
+        }
+
+        return sum;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,59 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-    }
+        String [][] correctArray = {
+                {"1", "2", "3", "4" },
+                {"5", "6", "7", "8" },
+                {"9", "10", "11", "12" },
+                {"13", "14", "15", "16" },
+        };
+
+        String [][] sizeArray = {
+                {"1", "2", "3", },
+                {"5", "6", "7", },
+                {"9", "10", "11", },
+        };
+
+        String [][] dataArray = {
+                {"1", "2", "3", "v" },
+                {"5", "g", "7", "8" },
+                {"9", "10", "11", "u" },
+                {"13", "14", "h", "16" },
+        };
+
+        try {
+            int result = Array.Array(correctArray);
+            System.out.println("Сумма всех элементов массива: " + result);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера массива: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных в массиве: " + e.getMessage());
+        }
+
+        try {
+            int result = Array.Array(sizeArray);
+            System.out.println("Сумма всех элементов массива: " + result);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера массива: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных в массиве: " + e.getMessage());
+        }
+
+        try {
+            int result = Array.Array(dataArray);
+            System.out.println("Сумма всех элементов массива: " + result);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера массива: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных в массиве: " + e.getMessage());
+        }
+
+        int[] numbers = {1, 2, 3, 4, 5, 6,};
+        try {
+
+            System.out.println(numbers[10]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ошибка: выход за границы массива: " + e.getMessage());
+        }
+}
 }
